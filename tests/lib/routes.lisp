1552d7c1bdfc64c591661fb1947cce988f23d808
paku <paku@skyizwhite.dev>
2023-03-04 13:04:34 +0900

    Improve test

--- tests/lib/routes.lisp
+++ tests/lib/routes.lisp
@@ -7,17 +7,17 @@
 (deftest register-handlers-test
   (testing "Define routing for CRUD operations"
     (ok
-     (expands '(register-handlers *app*
-                ("/api"     ()
-                 ("/v1"     ()
-                  ("/users" (:get    #'list-users
-                             :post   #'create-user)
-                   ("/:id"  (:get    #'show-user
-                             :put    #'update-user
-                             :delete #'destroy-user))))))
-              '(setf
-                (jingle:route *app* "/api/v1/users" :method :get) #'list-users
-                (jingle:route *app* "/api/v1/users" :method :post) #'create-user
-                (jingle:route *app* "/api/v1/users/:id" :method :get) #'show-user
-                (jingle:route *app* "/api/v1/users/:id" :method :put) #'update-user
-                (jingle:route *app* "/api/v1/users/:id" :method :delete) #'destroy-user)))))
+     (expands
+      '(register-handlers *app*
+        ("/api/v1" ()
+         ("/users" (:get    #'list-users
+                    :post   #'create-user)
+          ("/:id"  (:get    #'show-user
+                    :put    #'update-user
+                    :delete #'destroy-user)))))
+      '(setf
+        (jingle:route *app* "/api/v1/users" :method :get) #'list-users
+        (jingle:route *app* "/api/v1/users" :method :post) #'create-user
+        (jingle:route *app* "/api/v1/users/:id" :method :get) #'show-user
+        (jingle:route *app* "/api/v1/users/:id" :method :put) #'update-user
+        (jingle:route *app* "/api/v1/users/:id" :method :delete) #'destroy-user)))))
